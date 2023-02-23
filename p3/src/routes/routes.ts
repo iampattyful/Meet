5553cf4a83780f970869a63de96d0d34fd6ed846
namFei <namfly@hotmail.com>
2023-02-23 18:20:31 +0800

    homeServiceSetup

--- p3/src/routes/routes.ts
+++ p3/src/routes/routes.ts
@@ -1,17 +1,28 @@
-import express from "express"
-
-class Routes{
-    routes:express.Router = express.Router()
+import express from "express";
+import { isLoggedInAPI } from "../guard";
+class Routes {
+  routes: express.Router = express.Router();
 }
 
-export abstract class UserRoutes extends Routes{
-    constructor(){
-        super()
-        this.routes.post('/login',this.login)
-        this.routes.post('/logout',this.logout)
-    }
+export abstract class UserRoutes extends Routes {
+  constructor() {
+    super();
+    this.routes.post("/login", this.login);
+    this.routes.post("/logout", this.logout);
+  }
 
-    public abstract login(req:express.Request,res:express.Response):any
-    public abstract logout(req:express.Request,res:express.Response):any
+  public abstract login(req: express.Request, res: express.Response): any;
+  public abstract logout(req: express.Request, res: express.Response): any;
 }
 
+export abstract class HomeRoutes extends Routes {
+  constructor() {
+    super();
+    this.routes.get("/homePageImage", isLoggedInAPI, this.homePageImage);
+  }
+
+  public abstract homePageImage(
+    req: express.Request,
+    res: express.Response
+  ): any;
+}
