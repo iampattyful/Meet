26fdbc3bf50d7fa8870659cde87603f7910706ce
ting510 <royal427@gmail.com>
2023-02-23 22:03:06 +0800

    enroll

---
+++ p3/public/enroll.js
@@ -0,0 +1,13 @@
+let enrollForm = document.querySelector(".enrollForm")
+enrollForm.addEventListener("submit",async e=>{
+    e.preventDefault()
+    const formData = new FormData(enrollment_form)
+    const res = await fetch("/enroll", {
+        method: "POST",
+        body: formData,
+      });
+    const res_json = await res.json()
+    if (res_json.isLogin) {
+        res.status(200).redirect("/meet")
+    }
+})
