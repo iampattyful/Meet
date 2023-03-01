48866ad86e9320c61e5a4061e8327dd60103398b
namFei <namfly@hotmail.com>
2023-03-01 16:38:04 +0800

    edit update v2

--- p3/public/edit_personal_info.js
+++ p3/public/edit_personal_info.js
@@ -1,1 +1,15 @@
-document.querySelector;
+//hight
+let hight = 160;
+let newHight = document.querySelector("#numOfHight");
+document.querySelector("#hight").addEventListener("mouseup", () => {
+  hight = parseInt(document.querySelector("#hight").value);
+  newHight.innerHTML = hight;
+});
+
+//weight
+let weight = 50;
+let newWeight = document.querySelector("#numOfWeight");
+document.querySelector("#weight").addEventListener("mouseup", () => {
+  weight = parseInt(document.querySelector("#weight").value);
+  newWeight.innerHTML = weight;
+});
