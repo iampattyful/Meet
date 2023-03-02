5f3d4997f4ebb777e9b557366ce54cf11baa933b
namFei <namfly@hotmail.com>
2023-03-02 14:12:52 +0800

    test

--- p3/src/seeds/p3.ts
+++ p3/src/seeds/p3.ts
@@ -93,7 +93,29 @@ export async function seed(knex: Knex): Promise<void> {
       drink: false,
     },
   ]);
-
+  await knex("tag").insert([
+    { user_id: d1.id, tag_name: "run" },
+    {
+      user_id: d2.id,
+      tag_name: "swim",
+    },
+    {
+      user_id: d2.id,
+      tag_name: "sleep",
+    },
+    {
+      user_id: d3.id,
+      tag_name: "basketball",
+    },
+    {
+      user_id: d4.id,
+      tag_name: "sleep",
+    },
+    {
+      user_id: d5.id,
+      tag_name: "eat",
+    },
+  ]);
   // user 1 and 2 liked each other,
   // user 1 liked user 3 but user 3 didn't like user 1
   // user 2 liked user 3 but user 3 didn't like user 2
@@ -140,30 +162,32 @@ export async function seed(knex: Knex): Promise<void> {
       liked_to: d1.id,
     },
   ]);
-  
-  let [g1, g2, g3, g4] = await knex("group").insert([
-    {
-      matched_user_id1: d1.id,
-      matched_user_id2: d2.id,
-    },
-    {
-      matched_user_id1: d1.id,
-      matched_user_id2: d3.id,
-    },
-    {
-      matched_user_id1: d1.id,
-      matched_user_id2: d4.id,
-    },
-    {
-      matched_user_id1: d1.id,
-      matched_user_id2: d5.id,
-    }
-  ]).returning("id");;
+
+  let [g1, g2, g3, g4] = await knex("group")
+    .insert([
+      {
+        matched_user_id1: d1.id,
+        matched_user_id2: d2.id,
+      },
+      {
+        matched_user_id1: d1.id,
+        matched_user_id2: d3.id,
+      },
+      {
+        matched_user_id1: d1.id,
+        matched_user_id2: d4.id,
+      },
+      {
+        matched_user_id1: d1.id,
+        matched_user_id2: d5.id,
+      },
+    ])
+    .returning("id");
 
   await knex("message").insert([
     {
       user_id: d1.id,
-      group_id:g1.id,
+      group_id: g1.id,
       message: "hi",
     },
   ]);
@@ -172,7 +196,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d2.id,
-      group_id:g1.id,
+      group_id: g1.id,
       message: "hi",
     },
   ]);
@@ -182,7 +206,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d2.id,
-      group_id:g1.id,
+      group_id: g1.id,
       message: "nice to meet u",
     },
   ]);
@@ -192,7 +216,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d3.id,
-      group_id:g2.id,
+      group_id: g2.id,
       message: "gd night",
     },
   ]);
@@ -202,7 +226,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d4.id,
-      group_id:g3.id,
+      group_id: g3.id,
       message: "gd morning",
     },
   ]);
@@ -212,7 +236,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d1.id,
-      group_id:g3.id,
+      group_id: g3.id,
       message: "long time no c",
     },
   ]);
@@ -222,7 +246,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d5.id,
-      group_id:g4.id,
+      group_id: g4.id,
       message: "yo",
     },
   ]);
@@ -232,7 +256,7 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("message").insert([
     {
       user_id: d1.id,
-      group_id:g4.id,
+      group_id: g4.id,
       message: "yo yo",
     },
   ]);
