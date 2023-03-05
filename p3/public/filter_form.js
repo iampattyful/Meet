f52791369ac4caeae2498defb2072b6f433d24e2
namFei <namfly@hotmail.com>
2023-03-05 21:52:55 +0800

    test

--- p3/public/filter_form.js
+++ p3/public/filter_form.js
@@ -2,6 +2,7 @@ let userCard = document.querySelector(".slider");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   filter_form_main();
+  main();
 });
 
 async function filter_form_main() {
@@ -14,6 +15,54 @@ async function filter_form_main() {
   await handleFilterFormHttpRequest(formatFormData);
 }
 
+
+//////////////////////////////////////////////////
+let login_form = document.querySelector(".login_form");
+let logout_form = document.querySelector(".logout_form");
+let filterBtn = document.querySelector(".btn");
+
+async function main() {
+  await getCurrentUser();
+  reg_logout_click_event();
+}
+function reg_logout_click_event() {
+  console.log(123);
+  logout_form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    let res = await fetch("user/logout", {
+      headers: {
+        "Content-type": "Application/json",
+      },
+      method: "POST",
+      body: "",
+    });
+
+    let res_json = await res.json();
+
+    user = res_json.data;
+
+    if (!res_json.isErr) {
+      render_all_form();
+      window.location.href = "/";
+    } else {
+      alert(res_json.errMess);
+    }
+  });
+}
+
+async function render_all_form() {
+  if (user.isLogin) {
+    login_form.classList.add("isHide");
+    logout_form.classList.remove("isHide");
+    filterBtn.classList.remove("isHide");
+  } else {
+    login_form.classList.remove("isHide");
+    logout_form.classList.add("isHide");
+    filterBtn.classList.add("isHide");
+  }
+}
+/////////////////////////////////////////////////////
 async function getCurrentUser() {
   let res = await fetch("user/getCurrentUser");
   let res_json = await res.json();
